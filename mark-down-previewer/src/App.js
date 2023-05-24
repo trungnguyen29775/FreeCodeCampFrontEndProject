ca6909a3921f1beede2dbadd296e3747656cd47f
Nguyễn Trần Minh Trung <91869278+trungnguyen29775@users.noreply.github.com>
2023-05-25 02:11:02 +0700

    Add Regex

--- mark-down-previewer/src/App.js
+++ mark-down-previewer/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactDOM } from 'react';
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input:''
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    };
+    this.handleChange  = this.handleChange.bind(this)
+  }
+  handleChange(event)
+  {
+    const h1Test = /\#.+/g;
+    const h2Test = /\#\#.+/g;
+    const h3Test = /\#\#\#.+/g;
+    const codeTest = /\`\`\`(.)*\`\`\`/ig
+    const boldTest = /\*\*(.)*\*\*/ig
+    const italicTest = /\_(.)*\_/ig
+    const italicBoldTest = /\*\*\_(.)*\_\*\*/ig
+    const crossTest = /\~\~(.)*\~\~/ig
+    const linkTest = /\[(.)+\]/ig
+    const tHeadTest = /(.)+\|/ig
+    const tHeadSubTest = /\-\-\-\-\-\-\-\-\-\-\-\- \|/g
+    const tRowTest = /(.)+\|/ig
+    const ulTest = /\- /g
+    const olTest=/\[0-9]+\. /g
+    const imageTest = /\!\[(.)+\]\((.)+\)/ig
+    this.setState({input:event.target.value},()=>
+    {
+      const inputString = this.state.input;
+      if(h1Test.test(inputString))
+      {
+        const h1Array = inputString.match(h1Test)
+        h1Array.map((item,key)=>
+        {
+          const h1Element = <h1 key={key}>{item.slice(1,item.length)}</h1>
+          ReactDOM.render(h1Element,document.getElementById('preview'))
+        })
+        
+      }
+    })
+    
+
+  }
+  render()
+  {
+    return (
+      <div className="App">
+      <textarea id="editor" value={this.state.input} onChange={this.handleChange} placeholder='Type Something'>
+        </textarea>
+      <div id="preview"></div>
+      <div>
+        {
+  
+        }
+      </div>
+      </div>
+    );
+  }
+  
 }
+
 
 export default App;
