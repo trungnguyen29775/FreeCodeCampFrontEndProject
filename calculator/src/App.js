1377e287ae60230df1ddee7515d19b9eec42fe71
Nguyễn Trần Minh Trung <91869278+trungnguyen29775@users.noreply.github.com>
2023-06-07 09:53:47 +0700

    Done calculator

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,25 +5,74 @@ class App extends React.Component {
   {
     super(props)
     this.state = {
-      expression:"0"
+      expression:"",
+      result:"0"
     }
-    this.handleClick = this.handleClick.bind(this)
-    
+    this.handleNumClick = this.handleNumClick.bind(this)
+    this.handelOperationClick = this.handelOperationClick.bind(this)
+    this.handleClearClick = this.handleClearClick.bind(this)
+    this.handleEqualClick = this.handleEqualClick.bind(this)
+    this.handleDecimalClick = this.handleDecimalClick.bind(this)
   }
-  handleClick(event)
+  handleNumClick(event)
   {
-    if(event.target.id!=="equals" && event.target.id!=="clear")
+    if(this.state.expression=="0" && event.target.id=="zero")
     {
-      
-      this.setState({expression: this.state.expression+event.target.innerText})
+      this.setState({expression:"0"})
     }
-    else if(event.target.id==="equals")
+    else
     {
-      this.setState({expression: eval(this.state.expression)})
+      this.setState({expression:this.state.expression+event.target.innerText})
     }
-    else if(event.target.id==="clear")
+
+  }
+
+  handelOperationClick(event)
+  {
+    this.setState({expression:this.state.expression+event.target.innerText})
+  }
+  handleClearClick(event)
+  {
+    this.setState({expression:""})
+  }
+  handleEqualClick(event)
+  {
+    const spaceTest = /\s/ig
+    console.log(spaceTest.test(this.state.expression))
+    const decimalTest = /.+\..\../
+    if(decimalTest.test(this.state.expression))
     {
-      this.setState({expression:""})
+      const temp = this.state.expression.match(decimalTest)
+      let result=""
+      let i=0;
+      for(i=0;i<this.state.expression.length;i++)
+      {
+        if(this.state.expression[i]==".")
+          break
+        result+=this.state.expression[i]
+      }
+      result+="."
+      i+=1;
+      for(i;i<this.state.expression.length;i++)
+      {
+        if(this.state.expression[i]!==".")
+          result+=this.state.expression[i]
+      }
+      this.setState({expression:result})
+    }
+    else
+    {
+      this.setState({expression:eval(this.state.expression)})
+    }
+  } 
+  handleDecimalClick()
+  {
+    if(this.state.expression[this.state.expression.length-1]==".")
+    {
+    }
+    else
+    {
+      this.setState({expression:this.state.expression+"."})
     }
   }
   render(){
@@ -33,30 +82,31 @@ class App extends React.Component {
         <div className='calculator-container'>
         <div className='screen-container'>
         <h1 id="display">{this.state.expression!==""?this.state.expression:0}</h1>
+
         </div>
-        <button id='clear' onClick={this.handleClick}>AC</button>
+        <button id='clear' onClick={this.handleClearClick}>AC</button>
         <div className='number-container'>
-        <button id="zero"  onClick={this.handleClick}>0</button>
-        <button id="one"  onClick={this.handleClick}>1</button>
-        <button id="two"  onClick={this.handleClick}>2</button>
-        <button id="three"  onClick={this.handleClick}>3</button>
-        <button id="four"  onClick={this.handleClick}>4</button>
-        <button id="five"  onClick={this.handleClick}>5</button>
-        <button id="six"  onClick={this.handleClick}>6</button>
-        <button id="seven"  onClick={this.handleClick}>7</button>
-        <button id="eight"  onClick={this.handleClick}>8</button>
-        <button id="nine"  onClick={this.handleClick}>9</button>
+        <button id="zero"  onClick={this.handleNumClick}>0</button>
+        <button id="one"  onClick={this.handleNumClick}>1</button>
+        <button id="two"  onClick={this.handleNumClick}>2</button>
+        <button id="three"  onClick={this.handleNumClick}>3</button>
+        <button id="four"  onClick={this.handleNumClick}>4</button>
+        <button id="five"  onClick={this.handleNumClick}>5</button>
+        <button id="six"  onClick={this.handleNumClick}>6</button>
+        <button id="seven"  onClick={this.handleNumClick}>7</button>
+        <button id="eight"  onClick={this.handleNumClick}>8</button>
+        <button id="nine"  onClick={this.handleNumClick}>9</button>
         </div>
         <div className='expression-container'>
         <div className='operator-container'>
-        <button id="add"  onClick={this.handleClick}>+</button>
-        <button id="subtract"  onClick={this.handleClick}>-</button>
-        <button id="multiply" onClick={this.handleClick}>*</button>
-        <button id="divide" onClick={this.handleClick}>/</button>
-        <button id="decimal" onClick={this.handleClick}>.</button>
+        <button id="add"  onClick={this.handelOperationClick}>+</button>
+        <button id="subtract"  onClick={this.handelOperationClick}>-</button>
+        <button id="multiply" onClick={this.handelOperationClick}>*</button>
+        <button id="divide" onClick={this.handelOperationClick}>/</button>
+        <button id="decimal" onClick={this.handleDecimalClick}>.</button>
         </div>
         
-        <button id="equals"  onClick={this.handleClick}>=</button>
+        <button id="equals"  onClick={this.handleEqualClick}>=</button>
         </div>
         
         </div>
--- clock/src/App.js
+++ clock/src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+    this.state=
+    {
+      breakLength:"5",
+      sessionLength:"25",
+      label:"Session",
+      time:"25"
+    }
+  }
+  breakDecreament()
+  {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  breakIncreament()
+  {
+
+  }
+  sessionIncreament()
+  {
+
+  }
+  sessionDecreament()
+  {
+    
+  }
+  render()
+  {
+    return (
+      <div className="App">
+        <div>
+        <h1 id="break-label">Break Length</h1>
+      <h1 id="break-length">{this.state.breakLength}</h1>
+      <button id="break-decreament">Break Decreament</button>
+      <button id="break-increment">Break Increment</button>
+        </div>
+      <div>
+      <h1 id ="session-label">Session Length</h1>
+      <h1 id="session-length">{this.state.sessionLength}</h1>
+      <button id="session-decreament">Session Decreament</button>
+      <button id="session-increment">Session Increment</button>
+      </div>
+      
+      <h1 id="time-label">{this.state.label}</h1>
+      <h1 id="time-left">{this.state.time}</h1>
+      <button id="start_loop">Start</button>
+      <button id="reset">Reset</button>
+      <audio id="beep" src='https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav'></audio>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
